e3217a0253c04b5578e82d7f7c4c859b2eaf5f52
Mikko Uromo <mikko.uromo@gmail.com>
2013-08-13 20:57:43 +0300

    horrible quick proto hack, mostly just to flex opengl muscles

--- protos/tilemap/src/main.cpp
+++ protos/tilemap/src/main.cpp
@@ -5,7 +5,6 @@
 #include <iostream>
 #include <fstream>
 
-
 #if defined(__APPLE__) || defined(MACOSX)
 #include <OpenGL/glu.h>
 #include <OpenGL/glext.h>
@@ -23,9 +22,10 @@
 using namespace std;
 
 void cleanup(void);       
+void buildLevel(void);
 
-static int wWidth = 640;
-static int wHeight = 480;
+static int wWidth = 800;
+static int wHeight = 600;
 static float angle = .0f;
 
 double timer = .0f;
@@ -34,6 +34,11 @@ double timedelta;
 
 Camera *cam;
 
+GLuint levelList;
+
+GLuint textureRock;
+GLuint textureWood;
+
 void display(void) {
     timer = clock() / (CLOCKS_PER_SEC / 1000.0);
     timedelta = (lastFrame < .0) ? .0 : timer-lastFrame;
@@ -41,9 +46,11 @@ void display(void) {
 
     glClearColor(0.1f, 0.2f, 0.4f, 1.f);
     glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
-
+    glLoadIdentity();
     cam->setView();
 
+    glCallList(levelList);
+   
     glutSwapBuffers();
     glutPostRedisplay();
 }
@@ -62,7 +69,7 @@ void keyboard(unsigned char key, int x, int y) {
         exit (0);
 
 	// TODO fix frame rate dependency
-#define CAM_SPEED 5.f
+#define CAM_SPEED .5f
     case 'w': cam->moveZ(CAM_SPEED); break;
     case 's': cam->moveZ(-CAM_SPEED); break;
     case 'a': cam->moveX(-CAM_SPEED); break;
@@ -81,6 +88,8 @@ void keyboard(unsigned char key, int x, int y) {
      cout << "pressed " << ((int)key) << endl;
         break;
     }
+
+printf("cam pos = %.2f %.2f %.2f\n", cam->pos.x, cam->pos.y, cam->pos.z);
 }
 
 void click(int button, int updown, int x, int y)  {
@@ -100,7 +109,7 @@ void reshape(int x, int y) {
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
     //glOrtho(0, 1, 1, 0, 0, 1); 
-    gluPerspective( 45.0, (GLfloat)(wWidth)/(GLfloat)(wHeight), 0.1f, 50000.0 );
+    gluPerspective( 60.0, (GLfloat)(wWidth)/(GLfloat)(wHeight), 0.1f, 50000.0 );
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
     glutPostRedisplay();
@@ -131,6 +140,108 @@ GLuint loadTexture(char* fname) {
     return ret;
 }
 
+// drawBox() from GLUT sources
+void texturedCube(GLfloat size) {
+    static GLfloat n[6][3] = {
+	{-1.0, 0.0, 0.0},
+	{0.0, 1.0, 0.0},
+	{1.0, 0.0, 0.0},
+	{0.0, -1.0, 0.0},
+	{0.0, 0.0, 1.0},
+	{0.0, 0.0, -1.0}
+    };
+    static GLint faces[6][4] = {
+
+	{0, 1, 2, 3},
+	{3, 2, 6, 7},
+	{7, 6, 5, 4},
+	{4, 5, 1, 0},
+	{5, 6, 2, 1},
+	{7, 4, 0, 3}
+    };
+    GLfloat v[8][3];
+    GLint i;
+
+    v[0][0] = v[1][0] = v[2][0] = v[3][0] = -size / 2;
+    v[4][0] = v[5][0] = v[6][0] = v[7][0] = size / 2;
+    v[0][1] = v[1][1] = v[4][1] = v[5][1] = -size / 2;
+    v[2][1] = v[3][1] = v[6][1] = v[7][1] = size / 2;
+    v[0][2] = v[3][2] = v[4][2] = v[7][2] = -size / 2;
+    v[1][2] = v[2][2] = v[5][2] = v[6][2] = size / 2;
+
+    for (i = 5; i >= 0; i--) {
+	glBegin(GL_QUADS);
+	glNormal3fv(&n[i][0]);
+	glTexCoord2f(0.0f, 0.0f);
+	glVertex3fv(&v[faces[i][0]][0]);
+	glTexCoord2f(1.0f, 0.0f);
+	glVertex3fv(&v[faces[i][1]][0]);
+	glTexCoord2f(1.0f, 1.0f);
+	glVertex3fv(&v[faces[i][2]][0]);
+	glTexCoord2f(0.0f, 1.0f);
+	glVertex3fv(&v[faces[i][3]][0]);
+	glEnd();
+    }
+}
+
+void buildLevel() {
+
+#define NUM_LEVELS 3
+
+    const char *level[NUM_LEVELS];
+    level[0] = 
+	"xxxxxxxx"
+	"xoooooox"
+	"xoooooox"
+	"xoooooox"
+	"xoooooox"
+	"xoooooox"
+	"xoooooox"
+	"xxxxxxxx";
+
+    level[1] = 
+	"xxxxxxxx"
+	"x______x"
+	"x______x"
+	"x______x"
+	"x______x"
+	"x______x"
+	"x______x"
+	"xxxxxxxx";
+
+    level[2] = 
+	"x______x"
+	"________"
+	"________"
+	"________"
+	"________"
+	"________"
+	"________"
+	"x______x";
+
+    levelList = glGenLists(1);
+    glNewList(levelList, GL_COMPILE);
+
+    for (int z = 0; z < NUM_LEVELS; z++) {
+	for (int y = 0, ofs = 0; y < 8; y++) {
+	    for (int x = 0; x < 8; x++, ofs++) {
+		
+		char ch = level[z][ofs];
+		if (ch == '_') continue;
+		if (ch == 'x') glBindTexture( GL_TEXTURE_2D, textureRock );
+		if (ch == 'o') glBindTexture( GL_TEXTURE_2D, textureWood );
+		
+		glPushMatrix();
+		glTranslatef((float)x, (float)y, (float)z);
+		//glutWireCube(1.0);
+		texturedCube(1.0);
+		glPopMatrix();
+	    }
+	}
+    }
+    glEndList();
+}
+
 int main(int argc, char** argv) {
 
     srand((unsigned)time(NULL));
@@ -151,6 +262,10 @@ int main(int argc, char** argv) {
     cout << "Running OpenGL version: " << glGetString(GL_VERSION) << endl;
     cout << "Extensions:" << endl << glGetString(GL_EXTENSIONS) << endl;
 
+    glEnable( GL_TEXTURE_2D );
+    textureRock = loadTexture("rock.jpg");
+    textureWood = loadTexture("wood.jpg");
+
     // enable culling
     glEnable(GL_DEPTH_TEST);
     //glEnable(GL_CULL_FACE);
@@ -158,7 +273,11 @@ int main(int argc, char** argv) {
     //glFrontFace(GL_CW);
 
     cam = new Camera();
-    cam->pos.z = 50.f;
+    cam->pos.x = 3.5f;
+    cam->pos.y = 3.5f;
+    cam->pos.z = 8.5f;
+
+    buildLevel();
 
     glutMainLoop();
 
