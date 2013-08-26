193860b17136c9ec148e4391f9cd17f7ca669564
Mikko Uromo <mikko.uromo@gmail.com>
2013-08-26 22:41:34 +0300

    simple dialogue tree proto
    includes libjson too...

---
+++ protos/dialogue/dialogue.cpp
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <vector>
+#include <map>
+#include <iostream>
+#include "libjson.h"
+
+std::vector<std::string> g_lines;
+std::map<std::string, int> g_keywords;
+
+char* readFile(const char *filename)
+{
+    char* buffer = 0;
+    long length;
+    FILE* f = fopen(filename, "rb");
+
+    if (f)
+    {
+	fseek (f, 0, SEEK_END);
+	length = ftell(f);
+	fseek (f, 0, SEEK_SET);
+	buffer = new char [length+1];
+	fread(buffer, 1, length, f);
+	buffer[length] = 0;
+	fclose(f);
+    }
+
+    return buffer;
+}
+
+void parseLines(const JSONNode& n) 
+{
+    JSONNode::const_iterator it = n.begin();
+    while (it != n.end()) 
+    {
+	g_lines.push_back(it->as_string());
+	++it;
+    }
+}
+
+void parseKeywords(const JSONNode& n)
+{
+    JSONNode::const_iterator it = n.begin();
+    while (it != n.end()) 
+    {
+	// stupid workaround, for some reason it->as_int() doesn't link!
+	g_keywords[it->name()] = atoi((it->as_string()).c_str());
+	++it;
+    }
+}
+
+void ParseJSON(const JSONNode& n)
+{
+    JSONNode::const_iterator it = n.begin();
+    while (it != n.end()) 
+    {
+	std::string nodeName = it->name();
+	if (nodeName == "lines")
+	    parseLines(*it);
+	else if (nodeName == "keywords")
+	    parseKeywords(*it);
+
+	//increment the iterator
+	++it;
+    }
+}
+
+int main(void)
+{
+    char* jsonData = readFile("test.json");
+
+    JSONNode dialogue = libjson::parse(jsonData);
+    ParseJSON(dialogue);
+
+    printf("Dialogue lines\n");
+    for (std::vector<std::string>::iterator it = g_lines.begin(); it != g_lines.end(); ++it) {
+	std::string s = (*it);
+	printf("\t%s\n", s.c_str());
+    }
+
+    printf("\nKeywords\n");
+    for (std::map<std::string, int>::iterator it = g_keywords.begin(); it != g_keywords.end(); ++it) {
+	printf("\t%s -> %s\n", it->first.c_str(), g_lines.at(it->second).c_str());
+    }
+
+    return 0;
+}
