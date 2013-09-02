7a188c8ad9295fcb494dfefa256d90aad34efa14
Sampo Lappalainen <sampo@umbrasoftware.com>
2013-09-02 09:51:51 +0300

    Fix for OSX premake scripts

--- build/premake/funcs.lua
+++ build/premake/funcs.lua
@@ -6,20 +6,18 @@ function projectsettings(_name)
 	location("build/" .. toolset)
 	objdir("intermediates/" .. _name)
 
+	configuration "Debug"
+	defines { "DEBUG", "_DEBUG" }
+	targetsuffix "_d"
+
+	configuration "Release"
+	flags "OptimizeSpeed"
+	defines "NDEBUG"
+
 	-- because of osx dylib path
 	configuration { "macosx", "SharedLib" }
 	targetdir("bin/Frameworks")
 	
-	-- TODO: get these from somewhere instead of assuming libumbraXYZ(32|64)(_d).dylib
-	configuration { "macosx", "SharedLib", "Release", "x32" }
-	linkoptions { "-install_name @executable_path/../Frameworks/libumbra" .. _name .."32.dylib" }
-	configuration { "macosx", "SharedLib", "Release", "x64" }
-	linkoptions { "-install_name @executable_path/../Frameworks/libumbra" .. _name .."64.dylib" }
-	configuration { "macosx", "SharedLib", "Debug", "x32" }
-	linkoptions { "-install_name @executable_path/../Frameworks/libumbra" .. _name .."32_d.dylib" }
-	configuration { "macosx", "SharedLib", "Debug", "x64" }
-	linkoptions { "-install_name @executable_path/../Frameworks/libumbra" .. _name .."64_d.dylib" }
-
 	configuration("windows", "not StaticLib")
 	targetdir("bin/windows/") 
 	
@@ -38,21 +36,17 @@ function solutionsettings(_name)
 	language "C++"
 	flags { "Symbols", "NoPCH", "NoEditAndContinue",
 	        "NoMinimalRebuild", "NoRTTI", "NoManifest" }
+	
 	configuration { "x32 or x64", "macosx" }
 		local sdk_path = "/Developer/SDKs/MacOSX10.6.sdk/"
 		buildoptions { "-isysroot " .. sdk_path, "-mmacosx-version-min=10.6"}
 		linkoptions {  "-isysroot " .. sdk_path, "-mmacosx-version-min=10.6"}
-	configuration "Debug"
-	defines { "DEBUG", "_DEBUG" }
-	targetsuffix "_d"
+	
 	configuration { "Debug", "x32" }
 	targetsuffix "32_d"
 	configuration { "Debug", "x64" }
 	targetsuffix "64_d"
 
-	configuration "Release"
-	flags "OptimizeSpeed"
-	defines "NDEBUG"
 	configuration { "Release", "x32" }
 	targetsuffix "32"
 	configuration { "Release", "x64" }
