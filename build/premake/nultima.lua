dc6cc834dac128e7f68262bbe9e208bbb4bbeda6
Mikko Uromo <mikko.uromo@gmail.com>
2013-09-05 00:17:41 +0300

    Added rapidjson

--- build/premake/nultima.lua
+++ build/premake/nultima.lua
@@ -4,15 +4,15 @@ projectsettings("Nultima")
 platforms{"native", "x32", "x64"}
 configuration {"Debug or Release"}
 kind "ConsoleApp"
-flags { "ExtraWarnings", "FatalWarnings" }
+--flags { "ExtraWarnings", "FatalWarnings" }
 
 configuration { "windows" }
 defines { "GLUT_BUILDING_LIB", "GLUT_NO_LIB_PRAGMA", "GLEW_STATIC" }
 files { "source/*.cpp", "source/*.h" }
-links { "glut", "opengl32", "glu32", "glew" }
-includedirs { "external/glut/include", "external/glew/include" }
+links { "glut", "opengl32", "glu32", "glew"}
+includedirs { "external/glut/include", "external/glew/include", "external/rapidjson" }
 
 configuration { "macosx" }
 files { "source/*.cpp", "source/*.h" }
 links { "GLUT.framework", "OpenGL.framework", "glew"}
-includedirs { "external/glut/include", "external/glew/include" }
+includedirs { "external/glut/include", "external/glew/include", "external/rapidjson"}
