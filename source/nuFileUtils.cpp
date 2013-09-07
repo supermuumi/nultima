17dee31ecd1a0516ea64e095bbc46d169b1bf042
Mikko Uromo <mikko.uromo@gmail.com>
2013-09-08 01:10:32 +0300

    removed debug

--- source/nuFileUtils.cpp
+++ source/nuFileUtils.cpp
@@ -14,7 +14,6 @@ char* FileUtils::readFile(std::string fname)
     {
         file.seekg(0, std::ios::end);
         long size = (long)file.tellg();
-        printf("readFile(): size=%ld\n", size);
         buffer = new char[size];
         file.seekg(0, std::ios::beg);
         file.read(buffer, size);
